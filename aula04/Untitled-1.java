ea61ecb1896aa9c38e2be05d782937d332790697
Renatoon <renfall2023@gmail.com>
2023-12-30 20:10:01 -0300

    Initial commit

---
+++ aula04/Untitled-1.java
@@ -0,0 +1,15 @@
+
+
+public interface Main {
+    jButton1.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+                trataEventoBt1ActionPerformed(evt); //método de tratamento
+
+            }
+
+        });
+
+}
+
